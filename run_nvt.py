3929027b313f826dfbd39b8737526220f13bd251
ThorKongstad <thorkongstadmadsn@hotmail.com>
2023-12-04 13:04:41 +0100

    Initial commit

---
+++ run_nvt.py
@@ -0,0 +1,72 @@
+import argparse
+import pickle
+import os
+from typing import Optional
+from dataclasses import dataclass, field
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_random
+
+import ase.db as db
+from ase import Atoms
+from ase.md.nvtberendsen import NVTBerendsen
+from ase import units
+
+from sqlite3 import OperationalError
+
+#from gpaw import GPAW, FermiDirac, PoissonSolver, Mixer
+#from gpaw.utilities import h2gpts
+
+@dataclass
+class iteration_counter:
+    i: int = field(default=0)
+
+    def __add__(self, other):
+        return self.i + other
+
+@retry(retry=retry_if_exception_type(OperationalError), stop=stop_after_attempt(5), wait=wait_random(min=60, max=600)) # will retry randomly within 10 min
+def db_observer(atoms: Atoms, database_dir: str, temperature: float, time_step_size: float, calc_pickle: Optional[bytes] = None,):
+    mean_elec_pot_z = atoms.calc.get_electrostatic_potential().mean(1).mean(0)
+    fermi_E = atoms.calc.get_fermi_level()
+    work_function_top = mean_elec_pot_z[-1 if not (atoms.calc.mode if isinstance(atoms.calc.mode, str) else atoms.calc.mode.get('name')).lower() == 'pw' else -3] - fermi_E
+    work_function_bot = mean_elec_pot_z[0 if not (atoms.calc.mode if isinstance(atoms.calc.mode, str) else atoms.calc.mode.get('name')).lower() == 'pw' else 2] - fermi_E
+    with db.connect(database_dir) as db_obj:
+        cur_time = db_obj.get(selection='id=-1').get('time') + time_step_size
+        db_obj.write(atoms=atoms, kinitic_E=atoms.get_kinetic_energy(), fermi_E=fermi_E, work_top=work_function_top, work_bot=work_function_bot, temperature=temperature, time=cur_time, time_step_size=time_step_size, data=dict(calc_pickle=(calc_pickle if calc_pickle else pickle.dumps(atoms.calc))))
+
+
+def main(md_db: str, n_steps):
+    if not os.path.basename(md_db) in os.listdir(db_path if len(db_path := os.path.dirname(md_db)) > 0 else '.'): raise FileNotFoundError("Can't find database")
+    with db.connect(md_db) as db_obj:
+        row = db_obj.get(selection=f'id=-1')
+        atoms: Atoms = row.toatoms()
+        calc_pickle = row.data.get('calc_pickle')
+        atoms.set_calculator(pickle.load(calc_pickle))
+
+        #start_time = row.get('time')
+        time_step = row.get('time_step_size')
+
+        free_atoms = len(atoms) - sum([len(con.indices) for con in atoms.constraints]) # this will only work in newer version of ase and older fixatoms in older version, since indicies are called a in some places
+
+        temperature = row.get('temperature')
+        brendsen_tau = row.get('brendsen_tau')
+
+    dyn = NVTBerendsen(atoms, time_step * units.fs, free_atoms * temperature / len(atoms), brendsen_tau * units.fs, True)
+    dyn.attach(db_observer,
+               atoms=atoms,
+               database_dir=md_db,
+               temperature=temperature,
+               time_step_size=time_step,
+               calc_pickle=calc_pickle,
+               )
+
+    dyn.run(n_steps)
+
+
+if __name__ =='__main__':
+    parser =argparse.ArgumentParser()
+    parser.add_argument('db_structure')
+    parser.add_argument('calc_n_steps')
+    args = parser.parse_args()
+
+
+    main(args.db_structure, args.calc_n_steps)
+
---
+++ set_md_start_db.py
@@ -0,0 +1,59 @@
+import argparse
+import pickle
+
+from ase.io import read
+import ase.db as db
+
+from gpaw import GPAW, FermiDirac, PoissonSolver, Mixer
+from gpaw.utilities import h2gpts
+
+
+def main(structure: str, mode: str, xc: str, temperature: float, brendsen_tau: float, time_step: float, kpts: tuple[int, int, int] = (1, 1, 1),):
+    atoms = read(structure)
+
+    name = structure.split('/')[-1].split('.')[0]
+
+    calc = GPAW(
+        mode=mode,
+        basis='dzp',
+        #setups={'Pt': '10'},
+        xc=xc,
+        kpts=kpts,
+        occupations=FermiDirac(0.1),
+        poissonsolver={'dipolelayer': 'xy'},
+        mixer=Mixer(beta=0.025, nmaxold=5, weight=50.0),
+        gpts=h2gpts(0.18, atoms.cell, idiv=8),
+        #    convergence={'energy': 2.0e-7, 'density': 1e-5}, # HIGH
+        convergence={'energy': 5.0e-6, 'density': 8e-5},  # LOW
+        #    parallel = dict(sl_auto = True), #Using Scalapack = 4x speedup!
+        txt=f'{name}_{xc}_{mode}_k{"-".join(map(str, kpts))}.txt'
+    )
+
+    atoms.set_calculator(calc)
+
+    calc_pickle = pickle.dumps(calc)
+
+    with db.connect(name + '.db') as db_obj:
+        db_obj.write(atoms=atoms, kinitic_E=0, temperature=temperature, brendsen_tau=brendsen_tau, time=0, time_step_size=time_step, data=dict(calc_pickle=calc_pickle))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('traj_struc', type=str)
+    parser.add_argument('temperature', type=float)
+    parser.add_argument('brendsen_tau', type=float)
+    parser.add_argument('time_step_size', type=float)
+    parser.add_argument('XC', type=str)
+    parser.add_argument('-k', '--kpts', nargs=3, default=(1, 1, 1), type=int)
+    parser.add_argument('-m', '--mode', choices=('FD', 'LCAO', 'PW'), default='LCAO', type=str)
+    args = parser.parse_args()
+
+    main(
+        structure=args.traj_struc,
+        mode=args.mode,
+        xc=args.XC,
+        temperature=args.temperature,
+        brendsen_tau=args.brendsen_tau,
+        time_step=args.time_step_size,
+        kpts=args.kpts
+    )
