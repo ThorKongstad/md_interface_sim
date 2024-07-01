e8c6704e981e5a5d775693d1dea1eaf6b3dace0b
ThorKongstad <thorkongstadmadsn@hotmail.com>
2024-07-01 13:12:52 +0200

    added work_function plot

--- run_nvt.py
+++ run_nvt.py
@@ -5,19 +5,31 @@
 import argparse
 import pickle
 import os
+import sys
+import pathlib
 from typing import Optional
 from dataclasses import dataclass, field
 from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_random
+
+sys.path.insert(0, str(pathlib.Path(__file__).parent.parent))
+from md_interface_sim import folder_exist
+
+import numpy as np
 
 import ase.db as db
 from ase import Atoms
 from ase.md.nvtberendsen import NVTBerendsen
 from ase import units
+from ase.parallel import world, barrier
 
 from sqlite3 import OperationalError
 
 from gpaw import GPAW#, FermiDirac, PoissonSolver, Mixer
 #from gpaw.utilities import h2gpts
+
+import plotly.graph_objects as go
+import plotly.express as px
+
 
 @dataclass
 class iteration_counter:
@@ -26,6 +38,7 @@ class iteration_counter:
     def __add__(self, other):
         return self.i + other
 
+
 @retry(retry=retry_if_exception_type(OperationalError), stop=stop_after_attempt(5), wait=wait_random(min=60, max=600)) # will retry randomly within 10 min
 def db_observer(atoms: Atoms, database_dir: str, temperature: float, brendsen_tau:float, time_step_size: float, calc_par_pickle: Optional[bytes] = None,):
     mean_elec_pot_z = atoms.calc.get_electrostatic_potential().mean(1).mean(0)
@@ -33,8 +46,29 @@ def db_observer(atoms: Atoms, database_dir: str, temperature: float, brendsen_ta
     work_function_top = mean_elec_pot_z[-5] - fermi_E # [-1 if not (atoms.calc.parameters.get('mode') if isinstance(atoms.calc.parameters.get('mode'), str) else atoms.calc.parameters.get('mode').get('name')).lower() == 'pw' else -3] - fermi_E
     work_function_bot = mean_elec_pot_z[4] - fermi_E #[0 if not (atoms.calc.parameters.get('mode') if isinstance(atoms.calc.parameters.get('mode'), str) else atoms.calc.parameters.get('mode').get('name')).lower() == 'pw' else 2] - fermi_E
     with db.connect(database_dir) as db_obj:
+        global cur_time
         cur_time = db_obj.get(selection=f'id={len(db_obj)}').get('time') + time_step_size
         db_obj.write(atoms=atoms, kinitic_E=atoms.get_kinetic_energy(), fermi_E=fermi_E, work_top=work_function_top, work_bot=work_function_bot, temperature=temperature, brendsen_tau=brendsen_tau, time=cur_time, time_step_size=time_step_size, data=dict(calc_pickle=(str(calc_par_pickle) if calc_par_pickle else str(pickle.dumps(atoms.calc.parameters)))))
+
+
+def plot_work_functions(atoms: Atoms, calculation_name: str, time_step: float):
+    if world.rank == 0:
+        fermi_E = atoms.get_calculator.get_fermi_level()
+
+        mean_elec_pot_z = atoms.get_calculator.get_electrostatic_potential().mean(1).mean(0) - fermi_E
+        z_axis = np.linspace(0, atoms.cell[2, 2], len(mean_elec_pot_z), endpoint=False)
+
+        fig = px.line(
+            x=z_axis,
+            y=mean_elec_pot_z,
+        )
+
+        fig.update_layout(xaxis_title='Z', yaxis_title='work function')
+
+        folder_exist(f'workfunc_plots_{calculation_name}')
+        save_name = f'workfunc_plots_{calculation_name}/time_{time_step}'
+        fig.write_html(save_name + '.html', include_mathjax='cdn')
+    barrier()
 
 
 def main(md_db: str, n_steps: int):
@@ -47,13 +81,14 @@ def main(md_db: str, n_steps: int):
 
         #start_time = row.get('time')
         time_step = row.get('time_step_size')
+        cur_time = db_obj.get(selection=f'id={len(db_obj)}').get('time') + time_step
 
         free_atoms = len(atoms) - sum([len(con.index) for con in atoms.constraints]) # this will only work in newer version of ase and older fixatoms in older version, since indicies are called a in some places
 
         temperature = row.get('temperature')
         brendsen_tau = row.get('brendsen_tau')
 
-    dyn = NVTBerendsen(atoms, time_step * units.fs, free_atoms * temperature / len(atoms), brendsen_tau * units.fs, True)
+        dyn = NVTBerendsen(atoms, time_step * units.fs, free_atoms * temperature / len(atoms), brendsen_tau * units.fs, True)
     dyn.attach(db_observer,
                atoms=atoms,
                database_dir=md_db,
@@ -61,6 +96,13 @@ def main(md_db: str, n_steps: int):
                brendsen_tau=brendsen_tau,
                time_step_size=time_step,
                calc_par_pickle=calc_pickle,
+               )
+
+    dyn.attach(plot_work_functions,
+               interval=500,
+               atoms=atoms,
+               calculation_name=os.path.basename(md_db).split('.')[0],
+               time_step=cur_time
                )
 
     dyn.run(n_steps)
